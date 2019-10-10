30037ae2a248fa4b344bbf521c8e0861ca9b7c99
Gerardo <gera@hubs.mx>
2019-10-10 18:42:16 -0500

    Alta Proyecto

---
+++ src/components/global/Alert.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <b-alert
+            :show="dismissCountDown"
+            dismissible
+            :variant="mensaje.color"
+            @dismissed="dismissCountDown = 0"
+            @dismissed-count-down="countDownChanged"
+        >
+        {{mensaje.text}}
+        </b-alert>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return { 
+            mensaje: {color:'success', text: ''},
+            dismissSecs: 5,
+            dismissCountDown: 0
+        };
+    },
+    methods: {
+        countDownChanged(dismissCountDown){
+            this.dismissCountDown = dismissCountDown
+        },
+        showAlert(){
+            this.dismissCountDown = this.dismissSecs
+        }
+    }
+}
+</script>
---
+++ src/views/Cakes.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            cakes: [],
+        };
+    },
+    created(){
+        this.listaCakes();
+    },
+    methods: {
+        listaCakes(){
+            this.axios.get('cakes')
+            .then((resposne) => {
+                this.cakes = resposne.data;
+            })
+            .catch((e) => {
+                console.log('Error' + e);
+            })
+        }
+    }
+}
+</script>
